18ac109a5fb5f1f11b8c50a39c4a28cb545bb097
Jad Khalili <jad@videolab.ae>
2020-02-17 12:44:05 -0600

    About us, view cars homepage sections

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,5 @@
 imgLinks = ["../imgs/car-carousel-1.jpg", "../imgs/car-carousel-2.jpg", "../imgs/car-carousel-3.jpg"]
 
-$(document).ready(function(){
-	$("car-carousel").css()
-})
-
 $(".titlebar").click(function(){
 	$(".dropdown-container").animate({height: "toggle"})
 })
