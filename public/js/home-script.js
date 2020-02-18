659aeb5a1c9a877ce9cb5a3c0fcbd5dabc1ee16b
Jad Khalili <jad@videolab.ae>
2020-02-17 18:27:16 -0600

    Move home-specific JS to separate script

---
+++ public/js/home-script.js
@@ -0,0 +1,2 @@
+gsap.fromTo(".hero", {width: 0, opacity: 0}, {duration: 1.5, ease: "circ.out", opacity: 1, width: "100%"})
+gsap.fromTo(".hero-title", {opacity: 0}, {duration: 1, delay: 1.3, ease: "circ.out", opacity: 1})
