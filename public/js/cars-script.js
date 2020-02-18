28450c52d253a26aac023545ad96af8a71e49a36
Jad Khalili <jad@videolab.ae>
2020-02-17 21:28:41 -0600

    Animate cars page

---
+++ public/js/cars-script.js
@@ -0,0 +1,3 @@
+gsap.fromTo(".car-jaguar", {opacity: 0, x: "-100vw"}, {duration: 1, ease: "circ.out", delay: 0.2, opacity: 1, x: "0vw"})
+gsap.fromTo(".car-bear", {opacity: 0, x: "-100vw"}, {duration: 1, ease: "circ.out", delay: 0.8, opacity: 1, x: "0vw"})
+gsap.fromTo(".car-swan", {opacity: 0, x: "-100vw"}, {duration: 1, ease: "circ.out", delay: 1.4, opacity: 1, x: "0vw"})
