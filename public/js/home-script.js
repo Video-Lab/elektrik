07347192e7b80ae76de87e25a5f961d0e7e0bdec
Jad Khalili <jad@videolab.ae>
2020-02-17 18:43:14 -0600

    Finish home page animations

--- public/js/home-script.js
+++ public/js/home-script.js
@@ -1,2 +1,6 @@
 gsap.fromTo(".hero", {width: 0, opacity: 0}, {duration: 1.5, ease: "circ.out", opacity: 1, width: "100%"})
-gsap.fromTo(".hero-title", {opacity: 0}, {duration: 1, delay: 1.3, ease: "circ.out", opacity: 1})
+gsap.fromTo(".hero-title", {opacity: 0}, {duration: 0.5, delay: 0.7, ease: "circ.out", opacity: 1})
+gsap.fromTo(".company-vision-content", {x: "-50vw"}, {duration: 1, delay: 1.5, ease: "circ.out", x: "0vw"})
+gsap.fromTo(".company-vision-btn", {x: "50vw"}, {duration: 1, delay: 1.5, ease: "circ.out", x: "0vw"})
+gsap.fromTo(".car-carousel", {x: "100vw"}, {duration: 1, delay: 2.5, ease: "circ.out", x: "0vw"})
+gsap.fromTo(".contact-us", {opacity: 0}, {duration: 1, delay: 3.5, ease:"circ.out", opacity: 1})
