aef39a191de473b79e1e84fadf53cb5e825e01e4
Jad Khalili <jad@videolab.ae>
2020-02-17 20:46:23 -0600

    Animate rest of page, dropdown bug fix

--- public/js/about-us-script.js
+++ public/js/about-us-script.js
@@ -7,3 +7,6 @@ for(var i = 1; i < 4; i++) {
 	delayTime += 0.7
 	gsap.fromTo(id, {opacity: 0, y: "10vh"}, {duration: 1, delay: delayTime, ease:"circ.out", opacity: 1, y: "0vh"}) 
 }
+
+gsap.fromTo(".our-history", {opacity: 0, x: "-50vw"}, {duration: 1, delay: 2.9, ease:"circ.out", opacity: 1, x: "0vw"})
+gsap.fromTo(".our-mission", {opacity: 0, x: "50vw"}, {duration: 1, delay: 3.6, ease:"circ.out", opacity: 1, x: "0vw"})
