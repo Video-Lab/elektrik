38d6c0a1f4c4eace03da2f835d4752dce42c77a1
Jad Khalili <jad@videolab.ae>
2020-02-17 18:25:51 -0600

    Add animations to home - herp

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,8 @@
 imgLinks = ["../imgs/car-carousel-1.jpg", "../imgs/car-carousel-2.jpg", "../imgs/car-carousel-3.jpg"]
 
+gsap.fromTo(".hero", {width: 0, opacity: 0}, {duration: 1.5, ease: "circ.out", opacity: 1, width: "100%"})
+gsap.fromTo(".hero-title", {opacity: 0}, {duration: 1, delay: 1.3, ease: "circ.out", opacity: 1})
+
 $(".titlebar").click(function(){
 	$(".dropdown-container").animate({height: "toggle"})
 })
